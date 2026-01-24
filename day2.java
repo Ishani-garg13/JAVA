ddbc8e1632ca5b3351dc4c89b926a297241a2033
Ishani Garg <ishani13garg@gmail.com>
2026-01-24 18:52:03 +0530

    done

---
+++ day2.java
@@ -0,0 +1,8 @@
+class day2{
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        System.out.println("Sum="+(a+b));
+        System.out.println("Product="+(a*b));
+    }
+}
